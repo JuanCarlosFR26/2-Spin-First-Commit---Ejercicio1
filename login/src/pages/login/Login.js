63298016c631056218996a8448924cc788413e5d
Juan Carlos <jcf.daw@gmail.com>
2021-12-28 01:05:47 +0100

    version 1

---
+++ login/src/pages/login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from './Form/Form';
+import '../login/Login.css';
+
+
+const Login = () => {
+    return(
+        <div className='login'>
+            <Form />
+        </div>
+    )
+}
+
+export default Login;
